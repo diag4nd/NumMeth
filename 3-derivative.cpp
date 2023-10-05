cf3e11d49ac7570a9db2ed82e77404bc0134af2c
diag4nd <144257276+diag4nd@users.noreply.github.com>
2023-10-06 01:44:35 +0300

    Update 3-derivative.cpp

--- 3-derivative.cpp
+++ 3-derivative.cpp
@@ -34,6 +34,20 @@ long double rDeriv(int i, long double* x, long double* f)
   return (f[i+1] - f[i]) / h;
 }
 
+long double midDerivSecond(int i, long double *x, long double *f)
+{
+  long double h = x[1] - x[0];
+  if (i == 0)
+  {
+    return (2*f[i] - 5*f[i+1] + 4*f[i+2] - f[i+3])/(h*h);
+  }
+  else if (i == N-1)
+  {
+    return (2*f[i] - 5*f[i-1] + 4*f[i-2] - f[i-3])/(h*h);
+  }
+  return (f[i+1] - 2*f[i] + f[i-1])/(h*h);
+}
+
 long double midDerivDouble(int i, long double *x, long double *f)
 {
   long double h = x[1] - x[0];
@@ -66,7 +80,6 @@ int main()
         for (int i=0; i<N; i++)
         {
           df[i] = midDeriv(i, x, f);
-          std::cout << df[i] << std::endl;
           out << x[i] << '\t' << df[i] << std::endl;
         }  
     }
@@ -79,20 +92,30 @@ int main()
         for (int i=0; i<N; i++)
         {
           df[i] = rDeriv(i, x, f);
-          std::cout << df[i] << std::endl;
           out << x[i] << '\t' << df[i] << std::endl;
         } 
     }
     out.close();
     std::cout << std::endl;
-    
+
+    out.open("midDeriveSecond.txt");
+    if (out.is_open())
+    {
+        for (int i=0; i<N; i++)
+        {
+          df[i] = midDerivSecond(i, x, f);
+          out << x[i] << '\t' << df[i] << std::endl;
+        } 
+    }
+    out.close();
+    std::cout << std::endl;
+  
     out.open("midDerivDouble.txt");
     if (out.is_open())
     {
         for (int i=0; i<N; i++)
         {
           df[i] = midDerivDouble(i, x, f);
-          std::cout << df[i] << std::endl;
           out << x[i] << '\t' << df[i] << std::endl;
         }  
     }
